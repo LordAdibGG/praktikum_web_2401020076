94cf8d7b7d8cf9495255f95bac0120d0ed0040ad
Muhammad Adib Haryadi <muhammadadib58904@gmail.com>
2026-09-18 12:34:29 +0700

    Pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/latihan-php', function () {
+    $nama = 'Muhammad Adib Haryadi';
+    $nilai = [60, 55, 70, 50, 65];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+        return $total / count($data);
+    };
+
+    $rataRata = $hitungRataRata($nilai);
+
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
+});
